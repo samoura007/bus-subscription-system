9eb8e6bb6d5127c5bdeca426678566bbc4b6c241
Omar Soliman <omarahmkamal007@gmail.com>
2026-05-03 13:52:08 +0300

    Replaced the Dispatch system with a dynamic schedule system editable by the Admin. Also made the schedule for the users update dynamically. Fixed some small bugs in the Request Time Slot system

--- server/src/logic/DataStore.h
+++ server/src/logic/DataStore.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "../models/Models.h"
 #include <vector>
-#include <optional>
 #include <mutex>
-#include <algorithm>
+#include <optional>
+#include <string>
+#include <map>
 
 namespace bus {
 
@@ -11,9 +12,7 @@ class DataStore {
 public:
     DataStore();
 
-    bool addUser(const std::string& username,
-                 const std::string& passwordHash,
-                 const std::string& role);
+    bool addUser(const std::string& username, const std::string& passwordHash, const std::string& role);
     std::optional<User> findUserByUsername(const std::string& username) const;
     std::optional<User> findUserById(int id) const;
     std::vector<User> getAllUsers() const;
@@ -24,6 +23,7 @@ public:
     bool deleteRoute(int routeId);
     std::vector<Route> getAllRoutes() const;
     std::optional<Route> findRouteById(int routeId) const;
+    void updateRouteSchedule(int routeId, const std::string& schedule);
 
     bool subscribe(int userId, int routeId);
     bool unsubscribe(int userId, int routeId);
@@ -33,25 +33,24 @@ public:
     int issueTicket(int userId, int routeId, const std::string& timestamp, bool charged, int price);
     std::vector<Ticket> getTicketsForUser(int userId) const;
 
+    // Time Slot Requests
     void setVotes(int userId, const std::vector<RouteVote>& votes);
     std::vector<RouteVote> getVotes(int userId) const;
-    
     nlohmann::json getAggregatedDemand() const;
-    void fulfillDispatch(int routeId, const std::string& day, const std::string& timeSlot, const std::string& direction);
 
 private:
+    std::string generateDefaultSchedule(const std::string& routeName);
+
     mutable std::mutex m_mutex;
     std::vector<User>   m_users;
     std::vector<Route>  m_routes;
     std::vector<std::pair<int,int>> m_subscriptions;
     std::vector<Ticket> m_tickets;
-    std::vector<RouteVote> m_votes;
+    std::map<int, std::vector<RouteVote>> m_votes;
 
-    int m_nextUserId   = 1;
-    int m_nextRouteId  = 1;
+    int m_nextUserId = 1;
+    int m_nextRouteId = 1;
     int m_nextTicketId = 1;
-
-    void seedData();
 };
 
 } // namespace bus
--- shared/include/Messages.h
+++ shared/include/Messages.h
@@ -2,49 +2,50 @@
 
 namespace bus {
 
-// Auth
-inline constexpr const char* MSG_REGISTER  = "REGISTER";
-inline constexpr const char* MSG_LOGIN     = "LOGIN";
-inline constexpr const char* MSG_AUTH_OK   = "AUTH_OK";
-inline constexpr const char* MSG_AUTH_FAIL = "AUTH_FAIL";
+inline constexpr const char* MSG_REGISTER = "REGISTER";
+inline constexpr const char* MSG_LOGIN = "LOGIN";
+inline constexpr const char* MSG_AUTH_OK = "AUTH_OK";
 
-// Routes
-inline constexpr const char* MSG_GET_ROUTES  = "GET_ROUTES";
+// Route Management
+inline constexpr const char* MSG_GET_ROUTES = "GET_ROUTES";
 inline constexpr const char* MSG_ROUTES_LIST = "ROUTES_LIST";
-inline constexpr const char* MSG_ADD_ROUTE   = "ADD_ROUTE";
-inline constexpr const char* MSG_DEL_ROUTE   = "DEL_ROUTE";
-inline constexpr const char* MSG_ROUTE_OK    = "ROUTE_OK";
+inline constexpr const char* MSG_ADD_ROUTE = "ADD_ROUTE";
+inline constexpr const char* MSG_DEL_ROUTE = "DEL_ROUTE";
+inline constexpr const char* MSG_ROUTE_OK = "ROUTE_OK";
 
 // Subscriptions
-inline constexpr const char* MSG_SUBSCRIBE   = "SUBSCRIBE";
+inline constexpr const char* MSG_SUBSCRIBE = "SUBSCRIBE";
 inline constexpr const char* MSG_UNSUBSCRIBE = "UNSUBSCRIBE";
-inline constexpr const char* MSG_GET_SUBS    = "GET_SUBS";
-inline constexpr const char* MSG_SUBS_LIST   = "SUBS_LIST";
+inline constexpr const char* MSG_GET_SUBS = "GET_SUBS";
+inline constexpr const char* MSG_SUBS_LIST = "SUBS_LIST";
 
 // Rides & Tickets
-inline constexpr const char* MSG_RIDE        = "RIDE";
+inline constexpr const char* MSG_RIDE = "RIDE";
 inline constexpr const char* MSG_GET_TICKETS = "GET_TICKETS";
-inline constexpr const char* MSG_TICKETS     = "TICKETS";
+inline constexpr const char* MSG_TICKETS = "TICKETS";
 
-// Votes & Dispatch
-inline constexpr const char* MSG_SET_VOTES    = "SET_VOTES";
-inline constexpr const char* MSG_GET_VOTES    = "GET_VOTES";
-inline constexpr const char* MSG_VOTES_LIST   = "VOTES_LIST";
-inline constexpr const char* MSG_GET_DEMAND   = "GET_DEMAND";
-inline constexpr const char* MSG_DEMAND_LIST  = "DEMAND_LIST";
-inline constexpr const char* MSG_DISPATCH_BUS = "DISPATCH_BUS";
+// Schedule Management
+inline constexpr const char* MSG_UPDATE_SCHEDULE = "UPDATE_SCHEDULE";
+
+// Time Slot Requests (Demand/Voting)
+inline constexpr const char* MSG_SET_VOTES = "SET_VOTES";
+inline constexpr const char* MSG_GET_VOTES = "GET_VOTES";
+inline constexpr const char* MSG_VOTES_LIST = "VOTES_LIST";
+inline constexpr const char* MSG_GET_DEMAND = "GET_DEMAND";
+inline constexpr const char* MSG_DEMAND_LIST = "DEMAND_LIST";
 
 // Admin
-inline constexpr const char* MSG_GET_USERS  = "GET_USERS";
+inline constexpr const char* MSG_GET_USERS = "GET_USERS";
 inline constexpr const char* MSG_USERS_LIST = "USERS_LIST";
 inline constexpr const char* MSG_GET_USER_DETAIL = "get_user_detail";
-inline constexpr const char* MSG_USER_DETAIL     = "user_detail";
+inline constexpr const char* MSG_USER_DETAIL = "user_detail";
+
 // Generic
-inline constexpr const char* MSG_OK    = "OK";
+inline constexpr const char* MSG_OK = "OK";
 inline constexpr const char* MSG_ERROR = "ERROR";
 
 // Roles
-inline constexpr const char* ROLE_USER  = "user";
+inline constexpr const char* ROLE_USER = "user";
 inline constexpr const char* ROLE_ADMIN = "admin";
 
 } // namespace bus
